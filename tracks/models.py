38347bb92c350c9a8bc89551d3c8edc07ad655c3
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-08-30 18:59:56 +0500

    characters-list created

--- tracks/models.py
+++ tracks/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 
 class Track(models.Model):
+    character = models.CharField(max_length=100, default='anonym')
     title = models.CharField(max_length=100)
     artist = models.CharField(max_length=100)
     album = models.CharField(max_length=100)
     genre = models.CharField(max_length=100)
     release_date = models.DateField()
     cover_image = models.ImageField(upload_to='tracks_images/')
+    character_image = models.ImageField(upload_to='characters_images/')
     audio_file = models.FileField(upload_to='tracks_audio/')
